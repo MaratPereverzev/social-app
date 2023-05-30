b0499d2f8a14d5cbccfe5c6adb87e5ae29c4c343
Marat Pereverzev <marat.pereverzrv@gmail.com>
2023-05-30 19:34:24 +0300

    created a profile page

--- src/components/content-section/content-section.jsx
+++ src/components/content-section/content-section.jsx
@@ -4,6 +4,7 @@ import ProfilePage from './profile-page/profile-page';
 const ContentSection = () => {
     return (
         <div className={styles.ContentSection}>
+            <ProfilePage name='Oleg'/>
         </div>
     );
 }
---
+++ src/components/content-section/profile-page/profile-page.jsx
@@ -0,0 +1,12 @@
+import styles from './profile-page.module.css';
+import ProfileIntroBackground from './profile-intro-background/profile-intro-background';
+import ProfileInfo from './profile-info/profile-info';
+
+const ProfilePage = (props) => {
+    return (
+        <div className={styles.Profile_page}>
+        </div>
+    );
+}
+
+export default ProfilePage;
