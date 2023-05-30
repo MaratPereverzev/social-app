85caf48ec8165bdb0c9c9f5437dcf16dca37373e
Marat Pereverzev <marat.pereverzrv@gmail.com>
2023-05-30 19:31:44 +0300

    created a content section

---
+++ src/components/content-section/content-section.jsx
@@ -0,0 +1,11 @@
+import styles from './content-section.module.css';
+import ProfilePage from './profile-page/profile-page';
+
+const ContentSection = () => {
+    return (
+        <div className={styles.ContentSection}>
+        </div>
+    );
+}
+
+export default ContentSection;
