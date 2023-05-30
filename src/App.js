98b190cae362c76a5b49967c0272367c578859c9
Marat Pereverzev <marat.pereverzrv@gmail.com>
2023-05-30 19:30:05 +0300

    made a main container with display grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import MenuBar from './components/menu-bar/menu-bar';
+import ContentSection from './components/content-section/content-section';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <main className="App-content">
-        <MenuBar />
+        <div className='grid_container'>
+          <MenuBar />
+          <ContentSection />
+        </div>
       </main>
     </div>
   );
